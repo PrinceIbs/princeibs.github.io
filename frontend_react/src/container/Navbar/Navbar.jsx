3fc7aad7df3c5212a733984537f2bd61ae099879
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-03-25 17:21:13 +0100

    Completed base style for Navbar

--- frontend_react/src/container/Navbar/Navbar.jsx
+++ frontend_react/src/container/Navbar/Navbar.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from "react";
+import { HiMenu, HiMenuAlt1, HiMenuAlt4, HiX } from "react-icons/hi";
 
-import {images} from "../../constants";
-import "./Navbar.scss"
+import { images } from "../../constants";
+import "./Navbar.scss";
 
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = React.useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "skills", "work", "contact"].map((link) => (
+          <li className="app__flex p-text">
+            <a href={`#${link}`}>{link}</a>
+          </li>
+        ))}
+      </ul>
 
-export default Navbar
+      <div className="app__navbar-menu">
+        <HiMenu onClick={() => setIsNavOpen(true)} />
+        {isNavOpen && (
+          <div>
+            <HiX onClick={() => setIsNavOpen(false)} />
+            <ul>
+              {["home", "about", "skills", "work", "contact"].map((link) => (
+                <li key={link}>
+                  <a href={`#${link}`}>{link}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
